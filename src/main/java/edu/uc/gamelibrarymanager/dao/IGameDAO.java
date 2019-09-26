4321e3136c50324def986803990bb5bc4acb4a86
Samuel Curry <currysl@ucmail.uc.edu>
2019-09-26 16:54:21 -0400

    finished IGameDAO

--- src/main/java/edu/uc/gamelibrarymanager/dao/IGameDAO.java
+++ src/main/java/edu/uc/gamelibrarymanager/dao/IGameDAO.java
@@ -5,5 +5,6 @@ import edu.uc.gamelibrarymanager.dto.GameDTO;
 public interface IGameDAO {
     GameDTO fetchById(int id) throws Exception;
     GameDTO fetchGameBySteamId(String steamId) throws Exception;
-    
+    GameDTO create(GameDTO game) throws Exception;
+    GameDTO update(int id, GameDTO game) throws Exception;
 }
