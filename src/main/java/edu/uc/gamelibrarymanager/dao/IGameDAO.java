1fb2aa8e0750f6704195342ab72574de01b7255d
Samuel Curry <samc735@gmail.com>
2019-09-26 20:56:46 -0400

    finished documentation for DAOs

--- src/main/java/edu/uc/gamelibrarymanager/dao/IGameDAO.java
+++ src/main/java/edu/uc/gamelibrarymanager/dao/IGameDAO.java
@@ -8,8 +8,32 @@ import edu.uc.gamelibrarymanager.dto.GameDTO;
  * */
 
 public interface IGameDAO {
+    /**
+     * Fetches the game by ID
+     * @param id the id of the game to retrieve
+     * @throws Exception any problem in the underlying query
+     * */
     GameDTO fetchById(int id) throws Exception;
+
+    /**
+     * Fetches the game by Steam ID
+     * @param steamId the steam ID of the game to retrieve
+     * @throws Exception any problem in the underlying query
+     * */
     GameDTO fetchGameBySteamId(String steamId) throws Exception;
+
+    /**
+     * Creates the game with the given GameDTO object
+     * @param game the game to create
+     * @throws Exception any problem in the underlying persistence
+     * */
     GameDTO create(GameDTO game) throws Exception;
+
+    /**
+     * Updates the game by the given ID with the given GameDTO
+     * @param id the game to update
+     * @param game the game object to update the game with
+     * @throws Exception any problem in the underlying persistence
+     * */
     GameDTO update(int id, GameDTO game) throws Exception;
 }
