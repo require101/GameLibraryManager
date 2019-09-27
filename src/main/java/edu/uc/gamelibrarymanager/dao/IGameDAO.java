aaff800ebdbe885f0ffa092dc7e9f6c90b1d4740
Samuel Curry <samc735@gmail.com>
2019-09-26 20:49:12 -0400

    added documentation

--- src/main/java/edu/uc/gamelibrarymanager/dao/IGameDAO.java
+++ src/main/java/edu/uc/gamelibrarymanager/dao/IGameDAO.java
@@ -2,6 +2,11 @@ package edu.uc.gamelibrarymanager.dao;
 
 import edu.uc.gamelibrarymanager.dto.GameDTO;
 
+/**
+ * Data methods for the Game Object
+ * @author Samuel Curry
+ * */
+
 public interface IGameDAO {
     GameDTO fetchById(int id) throws Exception;
     GameDTO fetchGameBySteamId(String steamId) throws Exception;
--- src/main/java/edu/uc/gamelibrarymanager/dao/IUserDAO.java
+++ src/main/java/edu/uc/gamelibrarymanager/dao/IUserDAO.java
@@ -2,8 +2,31 @@ package edu.uc.gamelibrarymanager.dao;
 
 import edu.uc.gamelibrarymanager.dto.UserDTO;
 
+/**
+ * Data methods for the User Object
+ * @author Samuel Curry
+ * */
+
 public interface IUserDAO {
-    UserDTO fetchByUsername(String username) throws Exception;
+    /**
+     * Fetches the User by ID
+     * @param id the id of the user to retrieve
+     * @throws Exception any problem in the underlying query
+     * */
     UserDTO fetchById(int id) throws Exception;
+
+    /**
+     * Creates the User
+     * @param user the User object to create the UserDTO with
+     * @throws Exception any problem in the underlying persistence
+     * */
     UserDTO create(UserDTO user) throws Exception;
+
+    /**
+     * Tries to locate the user by username and password, returns null if nothing is found
+     * @param username the username of the user you are trying to locate
+     * @param password the password of the user you are trying to locate
+     * @throws Exception any problem in the underlying query
+     * */
+    UserDTO login(String username, String password) throws Exception;
 }
